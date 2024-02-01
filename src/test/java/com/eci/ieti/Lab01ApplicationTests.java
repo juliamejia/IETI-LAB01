a39140847c1679bc1e25f46da680109a07fb3a4b
julia <julia.mejia@mail.escuelaing.edu.co>
2024-01-31 20:33:32 -0500

    laboratorio1 de ieti Julia

---
+++ src/test/java/com/eci/ieti/Lab01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eci.ieti;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Lab01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
